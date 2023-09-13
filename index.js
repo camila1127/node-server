4d06fecefc7ace40b04c9784b049ff9bd7e5bc98
maria camila rodriguez <mariacamilarod1104@gmail.com>
2023-09-14 00:20:29 +0200

    Servidor lista de tareas

--- index.js
+++ index.js
@@ -1,155 +1,28 @@
-const readline = require('readline');
+// servidor HTTP
 
-// Creamos la interfaz para leer la entrada del usuario
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const http = require('http');
+const port = 3000;
+const host = "localhost";
 
-// Objeto que almacenará las tareas
-const tareas = {};
-
-// Función para mostrar el menú
-function mostrarMenu() {
-console.log(`\n--- Bienvenido a el menu de tareas --- \n
-1. Mostrar tareas pendientes\n
-2. Mostrar tareas Completadas\n
-3. Insertar tareas \n
-4. Eliminar tareas \n
-5. Completar tareas \n
-0. Salir \n 
----------------------------------
-`);
-rl.question('Seleccione una opción: ', (opcion) => {
-    ejecutarOpcion(opcion);
-  });
-}
-
-// Función para ejecutar la opción seleccionada por el usuario
-function ejecutarOpcion(opcion) {
-  switch (opcion) {
-    case '1':
-      mostrarTareasPendientes();
-      break;
-    case '2':
-      mostrarTareasCompletas();
-      break;
-    case '3':
-      agregarTarea();
-      break;
-    case '4':
-      eliminarTarea();
-      break;
-    case '5':
-      marcarTareaCompletada();
-      break;
-    case '0':
-    
-      rl.close();
-      break;
-    default:
-      console.log('Opción inválida. ');
-      mostrarMenu();
-      break;
-  }
-}
-function mostrarTareasPendientes() {
-  console.log('\n--- Lista de Tareas incompletas---\n');
-
-  if(Object.keys(tareas).length === 0){
-    console.log("No hay tareas Pendientes");
-    mostrarMenu();
-  }else{
-    for (let index in tareas){
-      if(!tareas[index].completada)
-        console.log(`ID: ${index}\nDescripción: ${tareas[index].descripcion} \n_________________`);
-}
-console.log('\n');
-
-  mostrarMenu();
-};
-}
-
-// Función para mostrar todas las tareas
-function mostrarTareasCompletas() {
-  console.log('\n--- tareas completadas  ---\n');
-  if(Object.keys(tareas).length === 0){
-    console.log("No hay tareas Completadas");
-    mostrarMenu();
-  }else{
-    for (let index in tareas){
-      if(tareas[index].completada)
-        console.log(`ID: ${index}\nDescripción: ${tareas[index].descripcion} \n_________________`);
-}  
-  console.log('\n');
-  mostrarMenu();
-};
-}
+// Definir la lista de tareas
+const tasks = [
+    { id: 1, descripcion: 'Organizar menu de la semana', estado: "pendiente" },
+    { id: 2, descripcion: 'Revisar los compromisos de la semana', estado: "completado" },
+    { id: 3, descripcion: 'Estudiar Node.js', estado: "pendiente" }
+  ];
 
 
-// Función para agregar una tarea
-function agregarTarea() {
-  rl.question('Ingrese la descripción de la tarea: ', (descripcion) => {
-    const id = Math.random().toString().substring(2);
-    tareas[id] = {
-      descripcion: descripcion,
-      completada: false
-    };
-    console.log(`Tarea agregada`);
-    mostrarMenu();
-  });
-}
+const server = http.createServer((req, res)=>{
+    if(req.method === 'GET' && req.url === '/tareas'){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(tasks));
+    }else{
+        res.statusCode = 404;
+        res.end('Recurso no encontrado');
+    } 
+}) 
 
-// Función para marcar una tarea como completada
+server.listen(port, host, ()=>{
+    console.log("Servidor encendido");
+})
 
-function marcarTareaCompletada() {
-  let num = 1;
-  let tareasPendientes = [];
-  if(Object.keys(tareas).length === 0){
-    console.log("No hay tareas Pendientes");
-    mostrarMenu();
-  }else{
-  for(let id in tareas){
-    if(!tareas[id].completada){
-    console.log(`${num}. ${tareas[id].descripcion}`)
-    num++;
-    tareasPendientes.push(id);
-    }
-  }
-}
-  rl.question('¿cual tarea desea completar?  ', (idtareas) => {
-    let idTareaEscogida = tareasPendientes[idtareas-1];
-    if (tareas[idTareaEscogida]) {
-      tareas[idTareaEscogida].completada = true;
-      console.log(`Tarea marcada como completada.`);
-    } else {
-      console.log('ID de tarea inválido.');
-    }
-     
-   mostrarMenu();
-  });
-}
-
-function eliminarTarea() {
-  let num = 1;
-  let allTareas= [];
-  for(let id in tareas){
-    console.log(`${num}. ${tareas[id].descripcion}`)
-    num++;
-    allTareas.push(id);
-  }
-  rl.question('¿cual tarea desea eliminar?  ', (idtareas) => {
-    let idTareaEscogida = allTareas[idtareas-1];
-    if(idtareas!=null){
-    delete tareas[idTareaEscogida];
-      console.log(`Tarea eliminada.`);
-    } else {
-      console.log('ID de tarea inválido.');
-    }
-     
-   mostrarMenu();
-  });
-}
-
-// Iniciamos el programa mostrando el menú
-mostrarMenu();
